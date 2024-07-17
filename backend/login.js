8cd47b2dfc5b82b620c9eff5e62ffc3297ba50ca
alejandro <115742704+kalech7@users.noreply.github.com>
2024-07-16 21:24:02 -0500

    Update login.js

--- backend/login.js
+++ backend/login.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
     origin: 'https://vertexcapital.today',
-    methods: ['GET', 'POST'], 
+    methods: ['GET', 'POST','FETCH'], 
     allowedHeaders: ['Content-Type'],
   }));
 
